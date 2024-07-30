cd4f6557802f7c4ffb4324c8daba3ed63c8fd40b
Pablo Velasquez <pabloavelasquez@gmail.com>
2024-07-30 11:36:28 -0500

    public

--- portfolio/views/footer.py
+++
@@ -1,9 +0,0 @@
-import reflex as rx
-from datetime import datetime as dt
-
-def footer() -> rx.Component:
-    return rx.hstack(
-        rx.text(f"© {dt.now().year} Pablo Velasquez", font_size="md"),
-        justify="center",
-        margin_top="50px",
-    )    
